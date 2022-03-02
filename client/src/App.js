e5ae80484bb566a1dc06c7ccca5fd3540d07bcba
solomonboundy1 <solomonboundy2017@gmail.com>
2022-03-02 10:21:02 +0000

    router has been set up with routes for signing up and logging in

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import "./App.css";
 import { Fragment } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 
 const App = () => (
-  <Fragment>
-    <Navbar />
-    <Landing />
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+    </Fragment>
+  </Router>
 );
 
 export default App;
