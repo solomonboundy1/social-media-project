268886f8c3daec5161995c70d49ba2bfa790b64a
solomonboundy1 <solomonboundy2017@gmail.com>
2022-03-14 16:06:51 +0000

    some dashboard login features added

--- client/src/components/dashboard/Dashboard.jsx
+++ client/src/components/dashboard/Dashboard.jsx
@@ -1,14 +1,41 @@
-import React, { useEffect } from "react";
+import React, { Fragment, useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getCurrentProfile } from "../../actions/profile";
+import Spinner from "../layout/Spinner";
+import { Link } from "react-router-dom";
 
-const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+const Dashboard = ({
+  getCurrentProfile,
+  auth: { user },
+  profile: { profile, loading },
+}) => {
   useEffect(() => {
     getCurrentProfile();
   }, [getCurrentProfile]);
 
-  return <div>Dashboard</div>;
+  return loading && profile === null ? (
+    <Spinner />
+  ) : (
+    <div className="container">
+      <Fragment>
+        <h1 className="large text-primary">Dashboard</h1>
+        <p className="lead">
+          <i className="fas fa-user">Welcome {user && user.name}</i>
+        </p>
+        {profile !== null ? (
+          <Fragment>has</Fragment>
+        ) : (
+          <Fragment>
+            <p>You have not setup a profile! Please add some info</p>
+            <Link to="/create-profile" className="btn btn-primary my-1">
+              Create Profile
+            </Link>
+          </Fragment>
+        )}
+      </Fragment>
+    </div>
+  );
 };
 
 Dashboard.propTypes = {
